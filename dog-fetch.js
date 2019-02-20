79e8c7ccf2a34e744ae8e15d7ad446db41907388
Ryan Cahela <ryancahela@gmail.com>
2019-02-19 21:41:42 -0800

    added display functionality

--- dog-fetch.js
+++ dog-fetch.js
@@ -12,16 +12,25 @@ $('form') .on('submit', function(event) {
 function getDogImages(numberOfDogs) {
     fetch(`https://dog.ceo/api/breeds/image/random/${numberOfDogs}`)
     .then(data => data.json())
-    .then(responseJson => console.log(responseJson))
+    .then(responseJson => {
+        console.log(responseJson.message);
+        renderDogs(responseJson.message);
+    })
     .catch(error => alert('Your dog cannot be found') 
 )};
 
-function createImgTag() {
-    
+function createImgTag(imageSource) {
+    return `<img src="${imageSource}" alt="picture of a dog">`;
 }
 
-function renderDogs() {
+function renderDogs(responseJson) {
+    let imgString = responseJson.map(createImgTag);
+    console.log(imgString.join(''));
 
+    let output = document.getElementById('output');
+    console.log(output)
+
+    output.innerHTML = imgString.join('');
 }
 
 
